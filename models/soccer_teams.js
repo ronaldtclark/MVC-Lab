f9d36903cdb6c56624500a818d995039679cc25d
Ron Clark <ronaldtclark@gmail.com>
2018-07-11 13:36:50 -0500

    created array

---
+++ models/soccer_teams.js
@@ -0,0 +1,27 @@
+const soccerTeams = [
+{
+  country: 'England',
+  manager: 'Gareth Southgate',
+  captain: 'Harry Kane'
+  won: true
+}, 
+{
+  country: 'Portugal',
+  manager: 'Fernando Santos',
+  captain: 'Cristiano Ronaldo',
+  won: true
+},
+{
+  country: 'France',
+  manager: 'Didier Deschamps',
+  captain: 'Hugo Lloris'
+  won: true
+},
+{
+  country: 'Croatia',
+  manager: 'Zlatko Dalic',
+  captian: 'Luka Modric',
+  won: false
+}
+]
+
